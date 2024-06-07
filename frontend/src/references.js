3479fd572d8906663723089463d03e77493aab0f
DiveDevHQ <132306893+DiveDevHQ@users.noreply.github.com>
2024-06-07 12:31:16 -0400

    Delete frontend directory

--- frontend/src/references.js
+++
@@ -1,5 +0,0 @@
-export const ChunkingTypes = Object.freeze([
-    { 'label': 'Per document', 'value': 'document' },
-    { 'label': 'Per paragraph', 'value': 'paragraph' },
-    { 'label': 'Custom', 'value': 'custom' }
-]);
